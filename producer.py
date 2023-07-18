f02ff28d7ec5e6e90caa2777ab634d7af7788f9b
skysky229 <nguyendinhthienhuy@gmail.com>
2023-07-18 20:05:24 +0700

    update spark, batches data are not saved

--- producer.py
+++ producer.py
@@ -4,27 +4,58 @@ from config import config
 from kafka import KafkaProducer
 import logging
 import sys
+import time
 
+"""df = pd.read_csv("data/markets_historical_vnindex_ind_test.csv")
+list_price = ["Price", "Open", "High", "Low"]
+for i in list_price:
+    df[i] = pd.to_numeric(df[i].apply(lambda x: x.replace(",", "")))
+
+BOOTSTRAP_SERVER = 'localhost'
+TOPIC = 'stock_pred'
+
+producer = KafkaProducer(bootstrap_servers=[BOOTSTRAP_SERVER])
+
+col = []
+for i in range(0, 7):
+    col.append(df.columns[i])
+
+def send(tmp):
+    '''s = ""
+    for i in range(0,6):
+        s = s + col[i] + ": " + str(tmp[i]) + "-"'''
+    s = {}
+    for i in range(0, 7):
+        s[col[i]] = tmp[i]
+    print("Message: ", s)
+    producer.send(TOPIC, value = json.dumps(s).encode('utf-8'))
+
+import time
+timing = 0.5
+for i in range(df.shape[0]-1, -1, -1):
+    tmp = list(df.loc[i])
+    send(tmp)
+    time.sleep(timing)"""
 
 def main():
      producer = KafkaProducer(bootstrap_servers=config['server'])
      topicName = config['topic']
 
-     df = pd.read_csv("data/markets_historical_vnindex_ind_test.csv")
+     df = pd.read_csv("data/markets_historical_vnindex_ind_test.csv").sort_values(by='Date')
      df = df[['Date', 'Price']]
 
      df['Price'] = pd.to_numeric(df['Price'].apply(lambda x: x.replace(",", "")))
-
-     df['json'] = df.apply(lambda x: x.to_json(), axis=1)
-     print(df['Date'].head())
-
+     
      for id, row in df.iterrows():
           #print(row['json'])
-          msg = json.dumps(row['json']).encode('utf-8')
+          msg = json.dumps(row.to_dict()).encode('utf-8')
           producer.send(topicName, msg)
           print(msg)
           print("Sent success ", id)
+          time.sleep(0.5)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     sys.exit(main())
+
+    
--- spark_predict.py
+++ spark_predict.py
@@ -1,20 +1,31 @@
 from tokenize import String
 from xml.dom.minicompat import StringTypes
 import pandas as pd
+import numpy as np
 import json
 from pyspark.sql import SparkSession
 from config import config
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from sklearn import preprocessing
+import os
+from tensorflow.keras.models import load_model
+import joblib
+
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-10_2.12:3.4.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1 pyspark-shell'
+
+out_df = pd.DataFrame(columns=['Date', 'Price', 'Prediction'])
+
+model = load_model("model/stock_prediction.h5")
 
 spark = SparkSession \
             .builder \
             .appName("APP") \
             .getOrCreate()
 
-df = spark \
-    .readStream \
+spark.sparkContext.setLogLevel("ERROR") # Only interested in ERROR-level logs; the others (such as WARN) will be ignored.
+
+df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", config['server']) \
     .option("subscribe", config['topic']) \
@@ -23,30 +34,32 @@ df = spark \
 
 df.printSchema()
 
-schema = ArrayType(StructType([
-    StructField("Date", StringType()), \
-    StructField("Price", DoubleType()), \
-]))
-
-
+inpSchema = StructType([
+    StructField("Date", StringType(), True), \
+    StructField("Price", DoubleType(), True), \
+])
 
 new_df = df.select(\
                 from_json(df.value.cast("string"),\
-                StructType().add("Date", StringType())\
-                            .add("Price", DoubleType())).alias("INFO")\
-                ,"key", "timestamp"
+                inpSchema, {"mode" : "PERMISSIVE"}).alias("INFO")
                 ).select("INFO.*")
 
 new_df.printSchema()
 
-
-out = new_df.writeStream \
-        .format("console") \
-        .outputMode("append") \
-        .start() \
+def prediction(row):
+    global out_df
+    if (out_df.shape[0] < 59):
+        out_df.loc[out_df.shape[0]] = [row['Date'], row['Price'], 0]
+    else :
+        pred_inp = out_df['Price'].tail(59).append(row['Price'])
+        pred_inp = np.reshape(pred_inp, (pred_inp.shape[0], 1))
+        pred_val = model.predict(pred_inp)
+        out_df.loc[out_df.shape[0]] = [row['Date'], row['Price'], pred_val]
+     
+    print(out_df, out_df.shape)
+out = new_df \
+        .writeStream \
+        .foreach(prediction) \
+        .trigger(processingTime='5 seconds') \
+        .start()  \
         .awaitTermination()
-
-
-
-
-        
