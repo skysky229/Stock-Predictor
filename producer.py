d8f2c06e39b8ef8c23df6bdb292d44f82b32f3b7
skysky229 <nguyendinhthienhuy@gmail.com>
2023-07-19 11:27:45 +0700

    change msg format

--- producer.py
+++ producer.py
@@ -10,9 +10,7 @@ def main():
      producer = KafkaProducer(bootstrap_servers=config['server'])
      topicName = config['topic']
 
-     df = pd.read_csv("data/markets_historical_vnindex_ind_test.csv").sort_values(by='Date')
-     df = df[['Date', 'Price']]
-
+     df = pd.read_csv("data/markets_historical_vnindex_ind_test.csv").set_index(["Date"]).filter(["Price"]).sort_values(by=["Date"])
      df['Price'] = pd.to_numeric(df['Price'].apply(lambda x: x.replace(",", "")))
      
      for i in range(60,len(df)):
@@ -20,7 +18,7 @@ def main():
           msg = json.dumps(chunk_df.to_dict()).encode('utf-8')
           producer.send(topicName, msg)
           print(msg)
-          print("Sent success ", id)
+          print("Sent success ", i)
           time.sleep(0.5)
 
 if __name__ == "__main__":
